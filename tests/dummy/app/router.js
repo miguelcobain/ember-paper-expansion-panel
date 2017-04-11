0f4b90f8b11996fd7f8ee97eb6d81654afba0d82
Miguel Andrade <jmandrade0@gmail.com>
2017-04-11 15:20:27 +0100

    use ERouter

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import config from './config/environment';
 const { Router: ERouter } = Ember;
 
-const Router = Router.extend({
+const Router = ERouter.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
