fbebb4491584722b124fc480f4456fc82bf45d02
Miguel Andrade <jmandrade0@gmail.com>
2017-04-11 15:17:22 +0100

    add suave lint

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import config from './config/environment';
+const { Router: ERouter } = Ember;
 
-const Router = Ember.Router.extend({
+const Router = Router.extend({
   location: config.locationType,
   rootURL: config.rootURL
 });
